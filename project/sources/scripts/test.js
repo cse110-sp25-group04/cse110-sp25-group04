e601f85395c77d5883d0aea9a37a7e24c7dbf135
MerujSargsyan <sargsyanmeruzhan48@gmail.com>
2025-05-08 22:22:08 -0700

    testing linter

---
+++ project/sources/scripts/test.js
@@ -0,0 +1,18 @@
+function BadFuncName() {
+
+}
+
+function goodFuncName (){
+    return "Bad Parens & quotes";
+}
+
+const GOOD_GLOBAL_NAME = 1;
+let BadVarName = 3;
+
+let badEquation=1+2;
+
+let badArrowFunc = () => {
+    return '';
+}
+
+console.log( 'bad param spacing' );
