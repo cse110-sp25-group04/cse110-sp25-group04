7d30bd9082c96e8abe5b070babb1538d3e270501
mazen-hamdi <mhamdi@ucsd.edu>
2025-05-11 19:44:06 -0700

    Fixed package.json formatting and added proper test cases

--- project/tests/app.test.js
+++ project/tests/app.test.js
@@ -13,3 +13,10 @@ function passingAdditionCheck() {
     const total = numA + numB;
     expect(total).toBe(7); // This assertion will pass
 }
+
+function demonstrationSuite() {
+    test('2 + 2 should be 5 (this test will fail)', failingAdditionCheck);
+    test('3 + 4 should be 7 (this test will pass)', passingAdditionCheck);
+}
+
+describe('Demonstration Suite', demonstrationSuite);
