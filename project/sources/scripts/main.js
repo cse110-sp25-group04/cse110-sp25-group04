cc8c3a5b28948b78b6e73f262ecd6877b7ec55a1
MerujSargsyan <sargsyanmeruzhan48@gmail.com>
2025-05-20 10:38:13 -0700

    changes requested

--- project/sources/scripts/main.js
+++ project/sources/scripts/main.js
@@ -1,13 +1,28 @@
 const COLS = 7;
 const ROWS = 7;
 
+/**
+ * CHANGES: PLACE GLOBAL VARIABLES IN AN INIT() FUNCTION
+ */
 let board = [];
 
+/**
+ * CHANGES: EXTRACT HELPER FUNCTIONS TO ANOTHER .js FILE
+ */
+/**
+ * CHANGES: DOCUMENTATION MUST BE DONE WITH JSDOC SYNTAX
+ */
 // Generate an empty board (ROWSxCOLS) and then render it
 // Each cell contains either 'grass', 'rock', or 'corrupt' tile 
 // and will be generated randomly with approximately the same probabilities
 function generateBoard() {
     const states = ['green', 'purple', 'orange'];
+    
+    /**
+     * NO ARROW FUNCTIONS!
+     * 
+     * The spacing between { legnth: ROWS } must be 0
+     */
     board = Array.from({ length: ROWS }, () => //Array(COLS).fill(null));
         Array.from({ length: COLS }, () => 
             states[Math.floor(Math.random() * states.length)]
@@ -26,6 +41,9 @@ function renderBoard() {
     const container = document.getElementById('grid-container');
     container.innerHTML = '';
 
+    /** 
+     * extract into a halper function and pass in ROWS and COLS
+     */
     for (let y = 0; y < ROWS; y++) {
         for (let x = 0; x < COLS; x++) {
             const cell = document.createElement('div');
@@ -48,6 +66,11 @@ function renderBoard() {
  * Generates inital empty board
  */
 function init() {
+    /**
+     * Place globals here and pass them around to the functions 
+     * to not corrupt the DOM
+     */
+    
     document.getElementById('new-board').addEventListener('click', generateBoard);
 
     document.getElementById('save-board').addEventListener('click', function () {
