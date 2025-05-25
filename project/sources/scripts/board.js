e454950ab91f5ed5fbf5d475d830579f69828105
Eric Nguyen <146395414+Ericyti@users.noreply.github.com>
2025-05-25 01:44:58 -0700

    Updated board.js to hold the skeleton of changeBoard function

---
+++ project/sources/scripts/board.js
@@ -0,0 +1,10 @@
+const ROWS = 4, COLS = 6;
+
+const board = Array.from({ length: ROWS }, () => Array(COLS).fill(null));
+
+function changeBoard(x, y, type) {
+
+}
+
+export { ROWS, COLS, board, changeBoard };
+export default { ROWS, COLS, board, changeBoard };
