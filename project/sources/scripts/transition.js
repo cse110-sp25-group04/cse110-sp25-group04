3e20f9f5b9b05f256e3a98f0ac000a6663bccda5
xiaojian1202 <chenx2003@gmail.com>
2025-06-06 17:30:22 -0700

    JS functionality to show/hide overlay

--- project/sources/scripts/transition.js
+++ project/sources/scripts/transition.js
@@ -1,3 +1,30 @@
 const modal = document.querySelector('.modal');
 const modalText = document.querySelector('.modal-text');
 const modalButton = document.querySelector('.modal-button');
+
+
+export function showModal(passed) {
+    if (passed) {
+        modalText.textContent = 'Level Completed!';
+        modalButton.textContent = 'NEXT LEVEL';
+    }
+
+    else {
+        modalText.textContent = 'Level Failed!';
+        modalButton.textContent = 'TRY AGAIN';
+    }
+    modal.classList.add('visible');
+    modal.classList.remove('hidden');
+}
+
+export function hideModal() {
+    modal.classList.remove('visible');
+    // Ensure the modal is hidden after the transition
+    window.setTimeout(function() {
+        modal.classList.add('hidden');
+    }, 300);
+}
+
+modalButton.addEventListener('click', function() {
+    hideModal();
+})
