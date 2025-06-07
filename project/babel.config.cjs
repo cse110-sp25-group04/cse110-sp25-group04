9b1bb8875611039ee50a54ba4200f6fe172c272e
RichardJ1 <notaspy1234@gmail.com>
2025-06-07 01:06:01 -0700

    missed one file with linter

--- project/babel.config.cjs
+++ project/babel.config.cjs
@@ -1,4 +1,4 @@
 // babel.config.cjs
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
 };
