f295b669a4f3847d3ce4d1801eb11149acc184a8
xiaojian1202 <chenx2003@gmail.com>
2025-06-07 02:06:37 -0700

    integrate confetti when level passed

--- project/sources/scripts/transition.js
+++ project/sources/scripts/transition.js
@@ -4,6 +4,12 @@ const modalText = document.getElementById('modal-text');
 const modalButton = document.getElementById('modal-button');
 let nextHandler = null;
 
+const confettiCanvas = document.getElementById('confetti-canvas');   
+const confettiCall = confetti.create(confettiCanvas, {
+    resize: true,
+    useWorker: true
+});
+
 export { showModal };
 
 function showModal(passed, nextLevel) {
@@ -13,6 +19,11 @@ function showModal(passed, nextLevel) {
     if (passed) {
         modalText.textContent = 'Level Completed!';
         modalButton.textContent = 'NEXT LEVEL'; 
+        confettiCall({
+            particleCount: 1200,
+            spread: 200,
+            origin: { y: 0.6 },
+        });
     }
 
     else {
